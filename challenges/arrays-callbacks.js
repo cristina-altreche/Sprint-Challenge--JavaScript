a2c25377d7824b75b4ea957843128cfb38f41208
Cristina Altreche <c.altreche@gmail.com>
2020-05-22 12:33:17 -0500

    stretch goals done

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -118,29 +118,18 @@ console.log(populationTotal);
  * The consume function should return the invocation of cb, passing a and b into cb as arguments
  */
 
-function consume(a, b, cb) {
-  return cb(a, b);
-}
+let consume = (a, b, cb) => cb(a, b);
 
 /* Step 2: Create several functions to callback with consume();
  * Create a function named add that returns the sum of two numbers
  * Create a function named multiply that returns the product of two numbers
  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
  */
-function add(num1, num2) {
-  return num1 + num2;
-}
-console.log(consume(1, 2, add));
+let add = (num1, num2) => num1 + num2;
 
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-console.log(consume(2, 2, multiply));
+let multiply = (num1, num2) => num1 * num2;
 
-function greeting(str1, str2) {
-  return `Hello ${str1} ${str2}, nice to meet you!`;
-}
-console.log(consume("Cristina", "Altreche", greeting));
+let greeting = (str1, str2) => `Hello ${str1} ${str2}, nice to meet you!`;
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 console.log(consume(2, 2, add)); // 4
@@ -150,5 +139,6 @@ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice t
 /*
 
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
+
 
 */
